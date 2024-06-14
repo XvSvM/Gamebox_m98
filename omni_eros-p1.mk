5285519c874f8af776bd3dce818a9551413cecb4
XvSvM <vadiagadia@gmail.com>
2024-06-14 13:13:49 +0300

    Update omni_eros-p1.mk

--- omni_eros-p1.mk
+++ omni_eros-p1.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from eros-p1 device
 $(call inherit-product, device/google/eros-p1/device.mk)
 
 PRODUCT_DEVICE := eros-p1
-PRODUCT_NAME := omni_eros-p1
+PRODUCT_NAME := twrp_eros-p1
 PRODUCT_BRAND := google
 PRODUCT_MODEL := GameBox
 PRODUCT_MANUFACTURER := google
