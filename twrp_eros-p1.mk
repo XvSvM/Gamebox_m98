16d4054ad0024b716a861f400d3fd0dffcc17cab
XvSvM <vadiagadia@gmail.com>
2024-07-16 18:15:03 +0300

    Update twrp_eros-p1.mk

--- twrp_eros-p1.mk
+++ twrp_eros-p1.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/google/eros-p1/device.mk)
 
 PRODUCT_DEVICE := eros-p1
-PRODUCT_NAME := twrp_eros-p1
+PRODUCT_NAME := twrp_eros
 PRODUCT_BRAND := google
 PRODUCT_MODEL := GameBox
 PRODUCT_MANUFACTURER := google
